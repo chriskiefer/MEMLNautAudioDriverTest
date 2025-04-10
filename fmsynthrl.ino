2b5f02190c10592cb11656d4f8652983139e1b4b
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2025-04-10 18:57:03 +0100

    Minimum example of working front panel

--- fmsynthrl.ino
+++ fmsynthrl.ino
@@ -1,10 +1,22 @@
 #include "src/memllib/audio/AudioDriver.hpp"
+#include "src/memllib/memlnaut/Pins.hpp"
+#include "src/memllib/memlnaut/MEMLNaut.hpp"
+#include "src/memllib/memlnaut/MEMLNautTest.hpp"
+
+#include <memory>
 
 volatile bool core1_ready = false;
 
 void setup()
 {
+    // Initialise main serial monitor
     Serial.begin(115200);
+    // Initialise pins
+    Pins::initializePins();
+    // Initialise MEMLNaut singleton
+    MEMLNaut::Initialize();
+    MEMLNautTest::Setup();
+
     Serial.println("Core 0: Waiting for Core 1 setup...");
     while (!core1_ready) {
         delay(1);
@@ -14,7 +26,15 @@ void setup()
 
 void loop()
 {
-
+    MEMLNaut::Instance()->loop();
+    delay(10);
+    
+    // Print a dot every second
+    static unsigned long lastPrint = 0;
+    if (lastPrint++ > 100) {
+        Serial.println(".");
+        lastPrint = 0;
+    }
 }
 
 void setup1()
